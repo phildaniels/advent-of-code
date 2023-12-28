c9a2c2e4cf9ae716d894f52e6e8c450257d7e2e5
Phillip Daniels <Phillip.Daniels@ensemblehp.com>
2023-12-28 15:07:53 -0700

    day 11

--- 2023/typescript/days/day11/part2/index.ts
+++ 2023/typescript/days/day11/part2/index.ts
@@ -9,14 +9,116 @@ try {
 } catch (e) {}
 
 const solve = () => {
-  console.log(input);
-  return 0;
+  const findEmtpyRowsAndColumns = (grid: string[]) => {
+    let m = grid.length;
+    let n = grid[0].length;
+    const emptyRowIndexes: number[] = [];
+    const emptyColumnIndexes: number[] = [];
+    for (let i = 0; i < m; i++) {
+      let includesGalaxy = false;
+      for (let j = 0; j < n; j++) {
+        const currentChar = grid[i][j];
+        if (currentChar === '#') {
+          includesGalaxy = true;
+          break;
+        }
+      }
+
+      if (!includesGalaxy) {
+        emptyColumnIndexes.push(i);
+      }
+    }
+    for (let i = 0; i < n; i++) {
+      let includesGalaxy = false;
+      for (let j = 0; j < m; j++) {
+        const currentChar = grid[j][i];
+        if (currentChar === '#') {
+          includesGalaxy = true;
+          break;
+        }
+      }
+
+      if (!includesGalaxy) {
+        emptyRowIndexes.push(i);
+      }
+    }
+
+    return [emptyColumnIndexes, emptyRowIndexes];
+  };
+
+  const findCoordinatesOfGalaxies = (
+    grid: string[],
+    emptyRowIndexes: number[],
+    emptyColumnIndexes: number[]
+  ) => {
+    let m = grid.length;
+    let n = grid[0].length;
+    const galaxyCoordinates: Array<[number, number]> = [];
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (grid[i][j] === '#') {
+          const rowsSmallerThanI = emptyRowIndexes.filter(
+            (index) => index < i
+          ).length;
+          const columnsSmallerThanJ = emptyColumnIndexes.filter(
+            (index) => index < j
+          ).length;
+          const adjustedI = i + rowsSmallerThanI * 999999;
+          const adjustedJ = j + columnsSmallerThanJ * 999999;
+          galaxyCoordinates.push([adjustedI, adjustedJ]);
+        }
+      }
+    }
+
+    return galaxyCoordinates;
+  };
+
+  const findShortestPathsBetweenAllGalaxies = (
+    coordinates: Array<[number, number]>
+  ) => {
+    const shortestPathsBetweenAllGalaxies: number[][] = [];
+    for (let i = 0; i < coordinates.length; i++) {
+      let shortestPaths: number[] = [];
+      const [startX, startY] = coordinates[i];
+      for (let j = i + 1; j < coordinates.length; j++) {
+        const [endX, endY] = coordinates[j];
+        shortestPaths.push(Math.abs(startX - endX) + Math.abs(startY - endY));
+      }
+
+      shortestPathsBetweenAllGalaxies.push(shortestPaths);
+    }
+
+    return shortestPathsBetweenAllGalaxies;
+  };
+
+  const lines = input.split('\n');
+  const [emtpyRowIndexes, emptyColumnIndexes] = findEmtpyRowsAndColumns(lines);
+
+  const galaxyCoordinates = findCoordinatesOfGalaxies(
+    lines,
+    emtpyRowIndexes,
+    emptyColumnIndexes
+  );
+  const shortestPathsBetweenAllGalaxies =
+    findShortestPathsBetweenAllGalaxies(galaxyCoordinates);
+
+  let sum = 0;
+  for (let i = 0; i < shortestPathsBetweenAllGalaxies.length; i++) {
+    const paths = shortestPathsBetweenAllGalaxies[i];
+    for (let j = 0; j < paths.length; j++) {
+      let path = paths[j];
+      sum += path;
+    }
+  }
+
+  return sum;
 };
 
-const answer = cachedAnswer ?? `${solve()}`;
-if (!cachedAnswer) {
-  const filePath = join(__dirname, 'answer.txt');
-  fs.writeFileSync(filePath, answer, { encoding: 'utf-8' });
-}
+const answer = `${solve()}`;
+// const answer = cachedAnswer ?? `${solve()}`;
+// if (!cachedAnswer) {
+//   const filePath = join(__dirname, 'answer.txt');
+//   fs.writeFileSync(filePath, answer, { encoding: 'utf-8' });
+// }
 
 export default answer;
