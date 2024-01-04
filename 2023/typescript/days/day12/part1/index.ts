245dafbcef2dc455aaf0e310b4b314bdf49305c5
Phillip Daniels <Phillip.Daniels@ensemblehp.com>
2024-01-04 16:23:57 -0700

    day 12

--- 2023/typescript/days/day12/part1/index.ts
+++ 2023/typescript/days/day12/part1/index.ts
@@ -9,15 +9,88 @@ try {
 } catch (e) {}
 
 const solve = () => {
-  console.log(input);
-  return 0;
+  const lines = input.split('\n');
+  const springsAndGroups: Array<[Array<'?' | '.' | '#'>, number[]]> = [];
+
+  const calculateValidArrangements = (
+    springs: Array<'?' | '.' | '#'>,
+    group: number[]
+  ): number => {
+    if (springs.length === 0 && group.length === 0) {
+      return 1;
+    }
+
+    if (springs.length === 0 && group.length !== 0) {
+      return 0;
+    }
+
+    const springHead = springs[0];
+    const springTail = springs.slice(1);
+
+    if (springHead === '.') {
+      return calculateValidArrangements(springTail, group);
+    }
+
+    if (springHead === '?') {
+      return (
+        calculateValidArrangements(['.', ...springTail], group) +
+        calculateValidArrangements(['#', ...springTail], group)
+      );
+    }
+
+    const groupHead = group[0];
+    const groupTail = group.slice(1);
+    const springsToGroup = springs.slice(0, groupHead) as Array<
+      '?' | '.' | '#'
+    >;
+
+    if (
+      !(
+        springsToGroup.length === groupHead &&
+        springsToGroup.every((spring) => spring !== '.')
+      )
+    ) {
+      return 0;
+    }
+
+    if (groupTail.length > 0 && springs[groupHead] === '#') {
+      return 0;
+    }
+
+    const springsAfterGroup = springs.slice(groupHead);
+    if (groupTail.length > 0) {
+      springsAfterGroup.splice(0, 1, '.');
+    }
+
+    return calculateValidArrangements(springsAfterGroup, groupTail);
+  };
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const [springString, groupString] = line.split(' ');
+    const _springs = springString.split('') as Array<'?' | '.' | '#'>;
+    const _group = groupString.split(',').map((char) => +char);
+    let springs: Array<'?' | '.' | '#'> = [];
+    let group: number[] = [];
+    for (let j = 0; j < 1; j++) {
+      springs = springs.concat(_springs);
+      group = group.concat(_group);
+    }
+    springsAndGroups.push([springs, group]);
+  }
+  let sum = 0;
+  for (let i = 0; i < springsAndGroups.length; i++) {
+    const [springs, group] = springsAndGroups[i];
+    const calculation = calculateValidArrangements(springs, group);
+    sum += calculation;
+  }
+  return sum;
 };
 
-// const answer = cachedAnswer ?? `${solve()}`;
-// if (!cachedAnswer) {
-//   const filePath = join(__dirname, 'answer.txt');
-//   fs.writeFileSync(filePath, answer, { encoding: 'utf-8' });
-// }
-const answer = solve();
+const answer = cachedAnswer ?? `${solve()}`;
+if (!cachedAnswer) {
+  const filePath = join(__dirname, 'answer.txt');
+  fs.writeFileSync(filePath, answer, { encoding: 'utf-8' });
+}
 
 export default answer;
--- 2023/typescript/days/day12/part2/index.ts
+++ 2023/typescript/days/day12/part2/index.ts
@@ -9,8 +9,102 @@ try {
 } catch (e) {}
 
 const solve = () => {
-  console.log(input);
-  return 0;
+  const lines = input.split('\n');
+  const springsAndGroups: Array<[Array<'?' | '.' | '#'>, number[]]> = [];
+  const calculateValidArrangements = (
+    springs: Array<'?' | '.' | '#'>,
+    group: number[],
+    cache: Map<string, number>
+  ): number => {
+    const cacheKey = `${springs.join('')} ${group.join(',')}`;
+    if (cache.has(cacheKey)) {
+      return cache.get(cacheKey)!;
+    }
+
+    let result: number;
+
+    if (springs.length === 0 && group.length === 0) {
+      return 1;
+    }
+
+    if (springs.length === 0 && group.length !== 0) {
+      return 0;
+    }
+
+    const springHead = springs[0];
+    const springTail = springs.slice(1);
+
+    if (springHead === '.') {
+      result = calculateValidArrangements(springTail, group, cache);
+      return result;
+    }
+
+    if (springHead === '?') {
+      result =
+        calculateValidArrangements(['.', ...springTail], group, cache) +
+        calculateValidArrangements(['#', ...springTail], group, cache);
+      cache.set(cacheKey, result);
+      return result;
+    }
+
+    const groupHead = group[0];
+    const groupTail = group.slice(1);
+    const springsToGroup = springs.slice(0, groupHead) as Array<
+      '?' | '.' | '#'
+    >;
+
+    if (
+      !(
+        springsToGroup.length === groupHead &&
+        springsToGroup.every((spring) => spring !== '.')
+      )
+    ) {
+      result = 0;
+      cache.set(cacheKey, result);
+      return result;
+    }
+
+    if (groupTail.length > 0 && springs[groupHead] === '#') {
+      result = 0;
+      cache.set(cacheKey, result);
+      return result;
+    }
+
+    const springsAfterGroup = springs.slice(groupHead);
+    if (groupTail.length > 0) {
+      springsAfterGroup.splice(0, 1, '.');
+    }
+
+    result = calculateValidArrangements(springsAfterGroup, groupTail, cache);
+    cache.set(cacheKey, result);
+    return result;
+  };
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const [springString, groupString] = line.split(' ');
+    const _springs = springString.split('') as Array<'?' | '.' | '#'>;
+    const _group = groupString.split(',').map((char) => +char);
+    let springs: Array<'?' | '.' | '#'> = [];
+    let group: number[] = [];
+    const copies = 5;
+    for (let j = 0; j < copies; j++) {
+      springs = springs.concat(_springs);
+      if (j !== copies - 1) {
+        springs.push('?');
+      }
+      group = group.concat(_group);
+    }
+    springsAndGroups.push([springs, group]);
+  }
+  let sum = 0;
+  const cache = new Map<string, number>();
+  for (let i = 0; i < springsAndGroups.length; i++) {
+    const [springs, group] = springsAndGroups[i];
+    const calculation = calculateValidArrangements(springs, group, cache);
+    sum += calculation;
+  }
+  return sum;
 };
 
 const answer = cachedAnswer ?? `${solve()}`;
