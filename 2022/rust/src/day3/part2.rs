de5a17ed589bf4e7697e5f342779a2fd3f531793
Phillip Daniels <Phillip.Daniels@ensemblehp.com>
2024-02-15 10:21:25 -0700

    day 3 boilerplate

---
+++ 2022/rust/src/day3/part2.rs
@@ -0,0 +1,7 @@
+use rust_advent_of_code_2022::get_input;
+
+pub fn solve() -> String {
+    let input = get_input("inputs/day3.txt");
+    println!("{}", input);
+    "0".to_string()
+}
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -8,9 +8,16 @@ pub mod day2 {
     pub mod part2;
 }
 
+pub mod day3 {
+    pub mod part1;
+    pub mod part2;
+}
+
 fn main() {
     println!("day1 part 1 {}", day1::part1::solve());
     println!("day1 part 2 {}", day1::part2::solve());
     println!("day2 part 1 {}", day2::part1::solve());
     println!("day2 part 2 {}", day2::part2::solve());
+    println!("day3 part 1 {}", day3::part1::solve());
+    println!("day3 part 2 {}", day3::part2::solve());
 }
